22eb141e87d7ead427864926dfac353ceb6aa9b4
Tony <ldhcjs@gmail.com>
2023-09-26 23:36:14 +0900

    Implement nativescript-community/ui-drawer.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'nativescript-vue';
 import Home from './components/Home.vue';
+import DrawerPlugin from '@nativescript-community/ui-drawer/vue3';
+import { install } from '@nativescript-community/ui-drawer';
+install();
 
-createApp(Home).start();
+createApp(Home)
+.use(DrawerPlugin)
+.start();
